90eb977efe75e2b6e016be4613a54c1de3ac995d
root <bfprietoc@unal.edu.co>
2019-10-25 00:01:31 -0500

    CORS

--- server.js
+++ server.js
@@ -3,12 +3,17 @@ const express = require ("express");
 const bodyParser = require("body-parser");
 const mysqlConnection = require("./connection");
 const PublicationsRoutes = require("./routes/products");
+const cors = require('cors');
+
 
 var app = express();
+
+
+app.use(cors());
 app.use(bodyParser.json());
 
 app.use("/products", PublicationsRoutes);
 
 
 
-app.listen(3400);
+app.listen(3000);
